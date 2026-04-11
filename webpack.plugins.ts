4ebed36d06f2b4385bf86c993298ae8280ecbac6
joshlin27 <joshua.qs.lin@gmail.com>
2026-04-10 20:32:28 -0700

    webpack

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,10 +1,21 @@
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import path from 'node:path';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
+  }),
+  new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: path.resolve(__dirname, 'public/turing-pattern.html'),
+        to: 'main_window/turing-pattern.html',
+      },
+    ],
   }),
 ];
